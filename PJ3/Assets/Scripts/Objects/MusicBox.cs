0cd18ef10fac7620016591a39229d7dc287f932b
Bernardo Leandro <80755587+BernardoLeandro1@users.noreply.github.com>
2024-05-24 00:49:29 +0100

    added new objects + bug fixing

--- PJ3/Assets/Scripts/Objects/MusicBox.cs
+++ PJ3/Assets/Scripts/Objects/MusicBox.cs
@@ -7,17 +7,26 @@ public class MusicBox : MonoBehaviour, IInteractable
     
     public GameObject door;
     public GameObject drawer;
+
+    public AudioClip music; 
+
+    private AudioSource audioPlayer;
+
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioPlayer = gameObject.transform.parent.transform.parent.gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     public bool Interact(GameObject currentObj)
     {
+        if(music!=null){
+            audioPlayer.clip = music;
+            audioPlayer.Play();
+        }
         transform.parent.GetComponent<Animator>().SetTrigger("OpenBox");
         door.GetComponent<Animator>().SetTrigger("OpenBox");
         drawer.GetComponent<Animator>().SetTrigger("OpenBox");
